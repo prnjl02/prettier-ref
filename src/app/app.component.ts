133d20046fde6e86aff8ffb33d8e1db113cd2d5e
pverma93 <pverma93@in.ibm.com>
2020-06-30 20:15:50 +0530

    tsemi

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from "@angular/core"
+import { Component } from "@angular/core";
 
 @Component({
   selector: "app-root",
@@ -6,9 +6,9 @@ import { Component } from "@angular/core"
   styleUrls: ["./app.component.css"],
 })
 export class AppComponent {
-  title = "prettier-ref"
-  app = "yess"
-  name = "pranjal"
-  lName = "verma"
-  id = 98
+  title = "prettier-ref";
+  app = "yess";
+  name = "pranjal";
+  lName = "verma";
+  id = 98;
 }
